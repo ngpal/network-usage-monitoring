167c374fc2ce55b9967bf4a1496f4d586037d9ba
ngpal <nandagopalnmenon@gmail.com>
2025-05-26 17:54:33 +0530

    basic packet logging

---
+++ br.sh
@@ -0,0 +1,1 @@
+cargo build && sudo RUST_LOG=info ./target/debug/network-usage-monitoring -i enp0s1
--- network-usage-monitoring-ebpf/src/main.rs
+++ network-usage-monitoring-ebpf/src/main.rs
@@ -5,59 +5,12 @@ use core::mem;
 
 use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
 use aya_log_ebpf::info;
-use network_types::eth::{EthHdr, EtherType};
-
-#[xdp]
-pub fn network_usage_monitoring(ctx: XdpContext) -> u32 {
-    match try_network_usage_monitoring(ctx) {
-        Ok(ret) => ret,
-        Err(_) => xdp_action::XDP_ABORTED,
-    }
-}
-
-fn et2str(et: EtherType) -> &'static str {
-    match et {
-        EtherType::Loop => "Loop",
-        EtherType::Ipv4 => "Ipv4",
-        EtherType::Arp => "Arp",
-        EtherType::Ipv6 => "Ipv6",
-        EtherType::FibreChannel => "FibreChannel",
-        EtherType::Infiniband => "Infiniband",
-        EtherType::LoopbackIeee8023 => "LoopbackIeee8023",
-    }
-}
-
-fn try_network_usage_monitoring(ctx: XdpContext) -> Result<u32, u32> {
-    let data = ctx.data();
-    let data_end = ctx.data_end();
-
-    let eth_hdr_size = mem::size_of::<EthHdr>();
-    if data + eth_hdr_size > data_end {
-        return Err(xdp_action::XDP_ABORTED);
-    }
-
-    let eth_hdr = unsafe { &*(data as *const EthHdr) };
-
-    let e_type = eth_hdr.ether_type;
-    let mac = eth_hdr.src_addr;
-    if mac == [0x76, 0xa6, 0xcd, 0xdc, 0xcb, 0x64] {
-        return Ok(xdp_action::XDP_PASS);
-    }
-
-    info!(
-        &ctx,
-        "src mac = {} {} {} {} {} {} type = {}",
-        mac[0],
-        mac[1],
-        mac[2],
-        mac[3],
-        mac[4],
-        mac[5],
-        et2str(e_type)
-    );
-
-    Ok(xdp_action::XDP_PASS)
-}
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::{IpProto, Ipv4Hdr},
+    tcp::TcpHdr,
+    udp::UdpHdr,
+};
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -65,6 +18,51 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-#[link_section = "license"]
-#[no_mangle]
-static LICENSE: [u8; 13] = *b"Dual MIT/GPL\0";
+#[xdp]
+pub fn xdp_firewall(ctx: XdpContext) -> u32 {
+    match try_xdp_firewall(ctx) {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
+
+#[inline(always)] //
+fn ptr_at<T>(ctx: &XdpContext, offset: usize) -> Result<*const T, ()> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return Err(());
+    }
+
+    Ok((start + offset) as *const T)
+}
+
+fn try_xdp_firewall(ctx: XdpContext) -> Result<u32, ()> {
+    let ethhdr: *const EthHdr = ptr_at(&ctx, 0)?; //
+    match unsafe { (*ethhdr).ether_type } {
+        EtherType::Ipv4 => {}
+        _ => return Ok(xdp_action::XDP_PASS),
+    }
+
+    let ipv4hdr: *const Ipv4Hdr = ptr_at(&ctx, EthHdr::LEN)?;
+    let source_addr = u32::from_be_bytes(unsafe { (*ipv4hdr).src_addr });
+
+    let source_port = match unsafe { (*ipv4hdr).proto } {
+        IpProto::Tcp => {
+            let tcphdr: *const TcpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            u16::from_be(unsafe { (*tcphdr).source })
+        }
+        IpProto::Udp => {
+            let udphdr: *const UdpHdr = ptr_at(&ctx, EthHdr::LEN + Ipv4Hdr::LEN)?;
+            u16::from_be_bytes(unsafe { (*udphdr).source })
+        }
+        _ => return Err(()),
+    };
+
+    //
+    info!(&ctx, "SRC IP: {:i}, SRC PORT: {}", source_addr, source_port);
+
+    Ok(xdp_action::XDP_PASS)
+}
